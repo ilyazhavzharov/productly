1696940d858657636755ead151ba17e2c764d9ce
Ilya <73776583+ilyazhavzharov@users.noreply.github.com>
2020-12-30 20:04:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,27 @@ $(function(){
   		slidesToShow: 3,
   		slidesToScroll: 3,
   		arrows: false,
-  		dots: true
+  		dots: true,
+  		responsive: [
+  		{
+  		breakpoint: 1175,
+  		settings: {
+  			slidesToShow: 2,
+  			slidesToScroll: 2,
+  			}
+  		},
+  		{
+  			breakpoint: 769,
+  	        settings: {
+  			slidesToShow: 1,
+  			slidesToScroll: 1,
+  			}
+  		}
+  	]
+});
+
+	$('.menu__btn').click(function(event) {
+		$('.menu__btn,.menu__list').toggleClass('active');
 	});
 
 });
